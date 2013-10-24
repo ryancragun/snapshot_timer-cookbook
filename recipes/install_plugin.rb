076c021ab98f4af740eb8643b22724e34b8e3843
Ryan Cragun <ryan@rightscale.com>
2013-10-24 15:48:13 -0700

    add return

--- recipes/install_plugin.rb
+++ recipes/install_plugin.rb
@@ -18,48 +18,44 @@
 
 rightscale_marker
 
-if node[:snapshot_timer][:enable_plugin] = "true"
+return unless node[:snapshot_timer][:enable_plugin] = "true"
 
-  rightscale_enable_collectd_plugin "exec"
-  include_recipe "rightscale::default"
-  include_recipe "rightscale::install_tools"
+rightscale_enable_collectd_plugin "exec"
+include_recipe "rightscale::default"
+include_recipe "rightscale::install_tools"
 
-  require 'fileutils'
+require 'fileutils'
 
-  log "   Installing snapshot_timer collectd plugin.."
+log "   Installing snapshot_timer collectd plugin.."
 
-  template(::File.join(node[:rightscale][:collectd_plugin_dir], "snapshot_timer.conf")) do
-    backup false
-    source "snapshot_timer.conf.erb"
-    notifies :restart, resources(:service => "collectd")
-    variables(
-      :collectd_lib => node[:rightscale][:collectd_lib],
-      :instance_uuid => node[:rightscale][:instance_uuid],
-      :lineage => node[:snapshot_timer][:lineage],
-      :interval => node[:snapshot_timer][:interval]
-    )
-  end
-
-  directory ::File.join(node[:rightscale][:collectd_lib], "plugins") do
-    action :create
-    recursive true
-  end
-
-  cookbook_file(::File.join(node[:rightscale][:collectd_lib], "plugins", 'snapshot_timer.rb')) do
-    source "snapshot_timer.rb"
-    mode "0755"
-    notifies :restart, resources(:service => "collectd")
-  end
-
-  file "/var/spool/cloud/user-data.rb" do
-    owner "root"
-    group "root"
-    mode "0664"
-    action :touch
-  end
-
-  log "   Installed collectd snapshot_timer plugin."
-
-else
-  log "   Plugin install is disabled.  Modify snapshot_timer/enable_plugin to enable it"
+template(::File.join(node[:rightscale][:collectd_plugin_dir], "snapshot_timer.conf")) do
+  backup false
+  source "snapshot_timer.conf.erb"
+  notifies :restart, resources(:service => "collectd")
+  variables(
+    :collectd_lib => node[:rightscale][:collectd_lib],
+    :instance_uuid => node[:rightscale][:instance_uuid],
+    :lineage => node[:snapshot_timer][:lineage],
+    :interval => node[:snapshot_timer][:interval]
+  )
 end
+
+directory ::File.join(node[:rightscale][:collectd_lib], "plugins") do
+  action :create
+  recursive true
+end
+
+cookbook_file(::File.join(node[:rightscale][:collectd_lib], "plugins", 'snapshot_timer.rb')) do
+  source "snapshot_timer.rb"
+  mode "0755"
+  notifies :restart, resources(:service => "collectd")
+end
+
+file "/var/spool/cloud/user-data.rb" do
+  owner "root"
+  group "root"
+  mode "0664"
+  action :touch
+end
+
+log "   Installed collectd snapshot_timer plugin."
